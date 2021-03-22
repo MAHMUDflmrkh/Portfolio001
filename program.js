c733ef1654e7fc4489ff96a5a204194ae4747d99
MAHMUDflmrkh <dronecr2@gmail.com>
2021-03-22 08:52:39 +0600

    portfolio files uploaded

---
+++ program.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.type', {
+  strings: ['Freelancer', 'Web Designer', 'WordPress Expert', 'Photographer', 'Blogger'],
+  smartBackspace: true,
+  startDelay: 1000,
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+});
